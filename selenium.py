4e555ccd838d9dd20de301ac93f57912e4f4742b
KHJ33 <hyunjo111@naver.com>
2022-09-22 00:49:47 +0900

    first commit

---
+++ selenium.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver_path = '/Users/kimhyunjo/Desktop/chromedriver'
+driver = webdriver.Chrome(driver_path)
