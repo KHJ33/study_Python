2c0da2a2ab6cdfcbd16f84aed60670ab9831720f
khj33 <hyunjo111@naver.com>
2022-12-06 22:04:38 +0900

    import graph.py

---
+++ graph.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import matplotlib 
+import pandas as pd
+import numpy as np
+
+matplotlib.rc('font', family='AppleGothic')  # mac 은 'AppleGothic'
+plt.rcParams['axes.unicode_minus'] = False  # - 기호 사용 가능
